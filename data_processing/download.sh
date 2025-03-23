2360220a9557bdb49c1cbd826691ba0e88e9a85e
Ming Hu <113792384+minghu0830@users.noreply.github.com>
2025-03-23 08:16:26 +0400

    Update download.sh

--- data_processing/download.sh
+++ data_processing/download.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-
+#  allow_patterns: OphNet2024_all,OphNet2024_trimmed_operation,OphNet2024_trimmed_phase,Features,Checkpoints
 python -c "
 from huggingface_hub import snapshot_download
 
 snapshot_download(
     repo_id='xioamiyh/OphNet2024',
     repo_type='dataset',
-    allow_patterns='OphNet2024_all/**',
+    # allow_patterns='OphNet2024_all/**',
+    # all untrimmed video
+
+    # allow_patterns='OphNet2024_localization/**',
+    # untrimmed videos annotated with temporal boundaries
+
+    # allow_patterns='OphNet2024_trimmed_operation/**',
+    # trimmed videos-operation level
+
+    allow_patterns='OphNet2024_trimmed_phase/**',
+    # trimmed videos-phase level
+
+    # allow_patterns='Checkpoints/**',
+
+    # allow_patterns='Features/**',
+
     local_dir='./'
 )
 "
